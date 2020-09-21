de139f8a5ac9daed668e557a19bcb871fbaa6858
Danny-coder <57795756+Danny-coder@users.noreply.github.com>
2020-09-21 18:58:46 +0200

    Update Call_Bomber.sh

--- Call_Bomber.sh
+++ Call_Bomber.sh
@@ -3,18 +3,16 @@ clear
                              echo -e "\e[4;31m Let me introduce \e[0m"
                               echo -e "\e[4;31m the best call bomb in all world \e[0m"
                                 echo -e "\e[4;31m Call_Bomber !!! \e[0m"
-echo "Premi invio per continuare!!"
 echo 'I requisiti si stanno installando'
 apt install figlet toilet python curl a -y
 pkg install pip
 pip install 3to2
 apt install python3-pip
 apt install python
-echo Questo script è stato creato da me!>update.danny
 
-echo Tutti i requisiti sono stati installati 
+echo "Tutti i requisiti sono stati installati"
 
-echo Per continuare premere il tasto invio!
+echo Press Enter to continue!
 read udp
 
 clear
@@ -25,6 +23,13 @@ echo Press Enter to continue...
 echo
 clear
 echo "Call_Bomber!!"
+
+
+
+
+
+
+
 
 echo -e "\e[ * \e[0m"                              
 echo -e "\e[ *\e[0m"                               
@@ -42,7 +47,7 @@ echo -e "\e[4;31m[|4 5 6   | \e[0m"
 echo -e "\e[4;31m[|7 8 9   | \e[0m"
 echo -e "\e[4;31m[|* 0 #   | \e[0m"
 
-echo "Scegli il numero:"
+echo "Chose a number"
 
-echo "1) Per poter effettuare le chiamate"
-
+echo "1) For doing Calls"
+echo "2) Exit "
