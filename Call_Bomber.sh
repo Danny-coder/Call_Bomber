bd0df33fdea1b08ca4b2598ac68b8209f8281d0b
Danny-coder <57795756+Danny-coder@users.noreply.github.com>
2020-09-21 18:36:55 +0200

    Update Call_Bomber.sh

--- Call_Bomber.sh
+++ Call_Bomber.sh
@@ -3,13 +3,14 @@ clear
 echo -e "Danny-Coder \e[5mBlink"
 echo -e "Present \e [5mBlink"
 echo -e "Call_Bomber \e [5mBlink"
-echo
-echo "requirements"
+echo 
+read a1
+echo " Installing requirements"
 
-apt install figlet
-apt install bash
+apt install figlet toilet python curl -y
 apt install python
 apt install git
+pip install -r requirements.txt
 
 echo "All right!"
 echo 
@@ -18,9 +19,9 @@ echo
 clear
 echo "Call_Bomber!!"
 
-echo -e "\e[ . \e[0m"                              
-echo -e "\e[ .\e[0m"                               
-echo -e "\e[ .\e[0m"
+echo -e "\e[ 0 \e[0m"                              
+echo -e "\e[ 0\e[0m"                               
+echo -e "\e[ 0\e[0m"
 echo -e "\e[|            \e[0m"
 echo -e "\e[ |             \e[0m"
 echo -e "\e[|____________     / /\e[0m"
@@ -28,7 +29,7 @@ echo -e "\e[[|           |   / /  \e[0m"
 echo -e "\e[[|           |  / /   \e[0m"
 echo -e "\e[[|           |  \ \   \e[0m"
 echo -e "\e[[|           |   \ \ \e[0m"
-echo -e "\e[  ___________     \ \ \e[0m"
+echo -e "\e[  ___________  | \e[0m"
 echo -e "\e[4;31m[|1 2 3   | \e[0m"
 echo -e "\e[4;31m[|4 5 6   | \e[0m"
 echo -e "\e[4;31m[|7 8 9   | \e[0m"
@@ -37,5 +38,4 @@ echo -e "\e[4;31m[|* 0 #   | \e[0m"
 echo "Scegli il numero:"
 
 echo "1) Per poter effettuare le chiamate"
-echo "2) Per uscire
-read udp
+
