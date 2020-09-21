f756912c69db6882983af76f2dc1d3aaae60dda2
Danny-coder <57795756+Danny-coder@users.noreply.github.com>
2020-09-21 18:26:52 +0200

    Create Call_Bomber.sh

---
+++ Call_Bomber.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+clear
+echo -e "Danny-Coder \e[5mBlink"
+echo -e "Present \e [5mBlink"
+echo -e "Call_Bomber \e [5mBlink"
+echo
+echo "requirements"
+
+apt install figlet
+apt install bash
+apt install python
+apt install git
+
+echo "All right!"
+echo 
+echo Press Enter to continue...
+echo
+clear
+echo "Call_Bomber!!"
+
+echo -e "\e[ . \e[0m"                              
+echo -e "\e[ .\e[0m"                               
+echo -e "\e[ .\e[0m"
+echo -e "\e[|            \e[0m"
+echo -e "\e[ |             \e[0m"
+echo -e "\e[|____________     / /\e[0m"
+echo -e "\e[[|           |   / /  \e[0m"
+echo -e "\e[[|           |  / /   \e[0m"
+echo -e "\e[[|           |  \ \   \e[0m"
+echo -e "\e[[|           |   \ \ \e[0m"
+echo -e "\e[  ___________     \ \ \e[0m"
+echo -e "\e[4;31m[|1 2 3   | \e[0m"
+echo -e "\e[4;31m[|4 5 6   | \e[0m"
+echo -e "\e[4;31m[|7 8 9   | \e[0m"
+echo -e "\e[4;31m[|* 0 #   | \e[0m"
+
+echo "Scegli il numero:"
+
+echo "1) Per poter effettuare le chiamate"
+echo "2) Per uscire
+read udp
